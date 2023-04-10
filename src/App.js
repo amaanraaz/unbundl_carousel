da9423f4ed3f6e96fc01cf0b5b67daf437cac000
amaanraaj <amandwivedi00@gmail.com>
2023-04-10 19:23:06 +0530

    caraousel code written using reactjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { urls } from './constants'; 
+import { useEffect, useState } from 'react';
+import leftArrow from "./assets/left-arrow.png";
+import rightArrow from "./assets/right-arrow.png";
 
 function App() {
+  // using a state variable to keep track of index to show coming from constants file.
+  const[index,setIndex] = useState(0);
+
+  // function to load previous image index
+  const prevLoad = ()=>{
+    index===0?setIndex(urls.large_screen.length-1):setIndex(index-1);
+  }
+
+  // function to load next image index
+  const nextLoad = ()=>{
+    index===urls.large_screen.length-1?setIndex(0):setIndex(index+1);
+  }
+
+  //  for auto load functionality
+  useEffect(()=>{
+    const load = setInterval(()=>{
+      nextLoad();
+    },2000);
+    return()=>clearInterval(load);
+  },[index])
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* image to show on larger screens */}
+      <div>
+        { <img src={urls.large_screen[index]} alt="img1" className='hidden md:block w-[100vw] h-[100vh] transition'/> }
+      </div>
+
+      {/* image to show on smaller screens */}
+      <div>
+        { <img src={urls.small_screen[index]} alt="img1" className='block md:hidden w-[100vw] h-[100vh]'/> }
+      </div>
+      
+      <div className='flex justify-between -mt-80'>
+        {
+          <img src={leftArrow} alt="img2" className='w-12 h-11 opacity-70 cursor-pointer' onClick={prevLoad}/>
+        }
+        {
+          <img src={rightArrow} alt="img2" className='w-12 h-11 opacity-70 cursor-pointer' onClick={nextLoad}/>
+        }
+      </div>
+    </>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const urls = {
+    large_screen:[
+        "https://cdn.shopify.com/s/files/1/0256/3683/1318/files/4_1_863f706a-f373-4c55-93ee-706498da3cd1.jpg?v=1678431210",
+        "https://cdn.shopify.com/s/files/1/0256/3683/1318/files/6_2.jpg?v=1678431231&width=1500",
+    ],
+    small_screen:[
+        "https://cdn.shopify.com/s/files/1/0256/3683/1318/files/4m_2.jpg?v=1679318063&width=375",
+        "https://cdn.shopify.com/s/files/1/0256/3683/1318/files/6m_2.jpg?v=1679318070&width=375"
+    ]
+}
